9dbf55e680ef04a9f791d15f420fcd15263a46da
victorien <deadvicmangame@gmail.com>
2022-11-21 15:10:43 +0100

    exo67

---
+++ app.js
@@ -0,0 +1,9 @@
+function maFonction(paragraphid,jaime_le_chocolat){
+let salut = document.getElementById(paragraphid);
+    salut.innerText = jaime_le_chocolat;
+}
+maFonction ("p1","jaime la fraise");
+maFonction ("p2","jaime la vanille");
+maFonction ("p3","jaime la framboise");
+maFonction ("p4","jaime la violette");
+maFonction ("p5","jaime la tomate");
